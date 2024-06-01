2702588dd08e26fc1cb1c1c3a3391fb3efe75a8a
kappy0713 <s22253@g.ashikaga.ac.jp>
2024-06-01 19:11:27 +0900

    feature：JWT認証でユーザーログイン

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"log"
 	"sports-backend/db"
 	"sports-backend/router"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db.Init()
 
 	router.Run()
